420fd26fee63365557dbcbaa1fe096128da32c4b
robdir <rhwickham@gmail.com>
2018-03-28 12:14:44 +0200

    appbar added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import PropTypes from 'prop-types'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import muiTheme from './styles/UI/theme'
+import Navigation from './components/Navigation'
 import './App.css';
 
 class App extends Component {
+  static childContextTypes = {
+    muiTheme: PropTypes.object.isRequired,
+  }
+
+  getChildContext() {
+    return { muiTheme }
+  }
+
   render() {
     return (
-      <div className="App">
-      </div>
+      <MuiThemeProvider muiTheme={muiTheme}>
+        <Navigation />
+          <div className="App">
+          </div>
+      </MuiThemeProvider>
     );
   }
 }
---
+++ src/components/Navigation.js
@@ -0,0 +1,26 @@
+import React, { PureComponent } from 'react'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+import { push } from 'react-router-redux'
+import AppBar from 'material-ui/AppBar'
+import IconButton from 'material-ui/IconButton'
+import MusicNote from 'material-ui/svg-icons/image/music-note'
+import FlatButton from 'material-ui/FlatButton'
+
+class Navigation extends PureComponent {
+
+    goHome() {
+        this.props.push('/')
+    }
+
+    render() {
+        return (
+            <AppBar
+                title="RhW Guitar Tuition"
+                iconElementLeft={<IconButton onClick={this.goHome}><MusicNote /></IconButton>}
+            />
+        )
+    }
+}
+
+export default Navigation
