d2844ba52da5d561c48700dc1e43692e5b24e168
robdir <rhwickham@gmail.com>
2018-03-28 15:17:06 +0200

    footsies

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,10 +8,12 @@ class Footer extends PureComponent {
             <div className="footer">
                 <div className="info">
                     <h3> Shameless self promotion </h3>
-                    <p> Not only am I a guitarist,
-                         I'm also a web-developer!
+                    <p>  I'm also a web-developer!
                          Check out my <a href="https://github.com/robdir"> 
                          Github!</a>
+                    </p>
+                    <p> Some of my music can be found:
+                         <a href="https://soundcloud.com/inuinuband/sets/demo"> here. </a>
                     </p>
                 </div>
             </div>
